03601a86e246b51802281aa2eb26611c70b6ad29
Jon Fox <jon@topaz.technology>
2018-10-02 18:01:30 +0100

    Added SQL Server sample docker-compose files

---
+++ bin/backup-sqlserver-volume.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Usage: backup-sqlserver-volume.sh <FULL PATH>
+docker run -it --rm -v compose_sqlserver-data:/data -v $1:/backup \
+    topaztechnology/base:3.6 tar -cjf /backup/topaz-sqlserver.tar.bz2 -C /data ./
